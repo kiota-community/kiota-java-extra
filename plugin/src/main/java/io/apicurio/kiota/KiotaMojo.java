f7daa342a0ae78902c7c6fa191b7b2a228ddde53
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-12-22 17:53:19 +0000

    a Kiota Maven Plugin

---
+++ plugin/src/main/java/io/apicurio/kiota/KiotaMojo.java
@@ -0,0 +1,241 @@
+package io.apicurio.kiota;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugin.logging.SystemStreamLog;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * This plugin will run Kiota to generate sources.
+ */
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class KiotaMojo extends AbstractMojo {
+    private final Log log = new SystemStreamLog();
+
+    /**
+     * Kiota executable target binary folder
+     */
+    @Parameter(required = true, defaultValue = "${project.build.directory}/kiota/")
+    private File targetBinaryFolder;
+
+    /**
+     * OS name
+     */
+    @Parameter(defaultValue = "${os.name}")
+    private String osName;
+
+    // Eventually make this configurable
+    private String osArch = "x64";
+
+    /**
+     * Base URL to be used for the download
+     */
+    @Parameter(defaultValue = "https://github.com/microsoft/kiota/releases/download")
+    private String baseURL;
+
+    /**
+     * Version of Kiota to be used
+     */
+    @Parameter(defaultValue = "0.9.0")
+    private String kiotaVersion;
+
+    // Kiota Options
+    /**
+     * The openapi specification to be used for generating code
+     */
+    @Parameter(defaultValue = "${basedir}/src/main/resources/openapi.json")
+    private File openapiSpec;
+
+    /**
+     * Location where to generate the Java code
+     */
+    @Parameter(defaultValue = "${project.build.directory}/generated-sources/kiota")
+    private File targetDirectory;
+
+    /**
+     * Language to generate the code for:
+     * <CSharp|Go|Java|PHP|Python|Ruby|Shell|Swift|TypeScript>
+     */
+    @Parameter(defaultValue = "Java")
+    private String language;
+
+    /**
+     * The class name to use for the core client class. [default: ApiClient]
+     *
+     */
+    @Parameter(defaultValue = "ApiClient")
+    private String clientClass;
+
+    /**
+     * The namespace to use for the core client class specified with the --class-name option. [default: ApiSdk]
+     *
+     */
+    @Parameter(defaultValue = "ApiSdk")
+    private String namespace;
+
+    /**
+     * Clean output before generating
+     *
+     */
+    @Parameter(defaultValue = "false")
+    private String cleanOutput;
+
+    /**
+     * Clear cache before generating
+     *
+     */
+    @Parameter(defaultValue = "false")
+    private String clearCache;
+
+    /**
+     * Kiota timeout in seconds
+     *
+     */
+    @Parameter(defaultValue = "30")
+    private int kiotaTimeout;
+
+    /**
+     * The log level of Kiota to use when logging messages to the main output. [default: Warning]
+     * <Critical|Debug|Error|Information|None|Trace|Warning>
+     */
+    @Parameter(defaultValue = "Warning")
+    private String kiotaLogLevel;
+
+    /**
+     * The current Maven project.
+     */
+    @Parameter(property = "project", required = true, readonly = true)
+    private MavenProject project;
+
+    @Override
+    public void execute() {
+        KiotaParams kp = new KiotaParams(osName, osArch);
+
+        downloadAndExtract(baseURL + "/v" + kiotaVersion + "/" + kp.downloadArtifact() + ".zip", targetBinaryFolder.getAbsolutePath(), kp);
+
+        executeKiota(Paths.get(targetBinaryFolder.getAbsolutePath(), kp.binary()).toFile());
+    }
+
+    private void executeKiota(File binary) {
+        List<String> cmd = new ArrayList<>();
+        cmd.add(binary.getAbsolutePath());
+        cmd.add("generate");
+        // process command line options
+        cmd.add("--openapi"); cmd.add(openapiSpec.getAbsolutePath());
+        cmd.add("--output"); cmd.add(targetDirectory.getAbsolutePath());
+        cmd.add("--language"); cmd.add(language);
+        cmd.add("--class-name"); cmd.add(clientClass);
+        cmd.add("--namespace-name"); cmd.add(namespace);
+        cmd.add("--clean-output"); cmd.add(cleanOutput);
+        cmd.add("--clear-cache"); cmd.add(clearCache);
+        cmd.add("--log-level"); cmd.add(kiotaLogLevel);
+
+        log.info("Going to execute the command: " + cmd.stream().collect(Collectors.joining(" ")));
+        Process ps = null;
+        try {
+            ProcessBuilder pb = new ProcessBuilder(cmd);
+            pb.directory(new File("."));
+            pb.inheritIO();
+            ps = pb.start();
+            ps.waitFor(kiotaTimeout, TimeUnit.SECONDS);
+            if (ps.exitValue() != 0) {
+                throw new RuntimeException("Error executing the Kiota command, return code is " + ps.exitValue());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to execute kiota", e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Failed to execute kiota", e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.getOutputStream().close();
+                } catch (IOException e) {
+                }
+                try {
+                    ps.getErrorStream().close();
+                } catch (IOException e) {
+                }
+                try {
+                    ps.getInputStream().close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        project.addCompileSourceRoot(targetDirectory.getAbsolutePath());
+    }
+
+    private void downloadAndExtract(String url, String dest, KiotaParams kp) {
+        try {
+            URL s = new URL(url);
+            File zipFile = Paths.get(dest, "kiota.zip").toFile();
+            File finalDestination = Paths.get(dest, kp.binary()).toFile();
+
+            if (!finalDestination.exists()) {
+                new File(dest).mkdirs();
+                ReadableByteChannel readableByteChannel = Channels.newChannel(s.openStream());
+                try (FileOutputStream fileOutputStream = new FileOutputStream(zipFile)) {
+                    fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+                }
+                try (FileSystem fileSystem = FileSystems.newFileSystem(zipFile.toPath(), this.getClass().getClassLoader())) {
+                    Path fileToExtract = fileSystem.getPath("/" + kp.downloadArtifact() + "/" + kp.binary());
+                    Files.copy(fileToExtract, finalDestination.toPath());
+                }
+                finalDestination.setExecutable(true, false);
+                zipFile.delete();
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Error downloading the Kiota release: " + url, e);
+        }
+    }
+
+    private static class KiotaParams {
+        final String osName;
+        final String osArch;
+
+        public KiotaParams(String osName, String osArch) {
+            this.osName = osName.toLowerCase(Locale.ROOT);
+            this.osArch = osArch;
+        }
+
+        public String downloadArtifact() {
+            if (osName.startsWith("mac") || osName.startsWith("osx")) {
+                return "osx-x64";
+            } else if (osName.startsWith("windows")) {
+                return "win-x64";
+            } else if (osName.startsWith("linux")) {
+                return "linux-x64";
+            } else {
+                throw new IllegalArgumentException("Detected OS is not supported: " + osName);
+            }
+        }
+
+        public String binary() {
+            if (osName.startsWith("windows")) {
+                return "kiota.exe";
+            } else {
+                return "kiota";
+            }
+        }
+    }
+}
