8b50e1297c5d20d2f80f5f16a649f07f0c7fc0dd
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-10-16 08:55:22 +0100

    updates and setup fixes

--- maven/plugin/src/main/java/io/kiota/maven/plugin/KiotaMojo.java
+++ maven/plugin/src/main/java/io/kiota/maven/plugin/KiotaMojo.java
@@ -69,7 +69,7 @@ public class KiotaMojo extends AbstractMojo {
     /**
      * Version of Kiota to be used
      */
-    @Parameter(defaultValue = "1.13.0")
+    @Parameter(defaultValue = "1.14.0")
     private String kiotaVersion;
 
     // Kiota Options
